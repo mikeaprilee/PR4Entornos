a0fe3e7552b0b0308696f324bc14f5a0f079687e
mikeaprilee <mikeaprilee@gmail.com>
2026-05-19 11:11:49 +0200

    Javadoc

--- src/main/java/PR4Entornos/Cajero.java
+++ src/main/java/PR4Entornos/Cajero.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.lorenzo.pr4entornos;
+package PR4Entornos;
 
 import java.util.ArrayList;
 
@@ -17,6 +17,10 @@ public class Cajero {
     double t;
     ArrayList<Producto> ps;
 
+    /**
+     * 
+     * @param n 
+     */
     public Cajero(String n) {
         this.n = n;
         this.c = 0;
--- src/main/java/PR4Entornos/Main.java
+++ src/main/java/PR4Entornos/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.lorenzo.pr4entornos;
+package PR4Entornos;
 
 /**
  *
